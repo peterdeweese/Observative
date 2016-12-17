66431129b3dbcc701bc69a0b07bf06d61acd1c1b
Peter DeWeese <peter@dewee.se>
2016-12-16 21:53:16 -0500

    Initial Commit

---
+++ Observatory/Observatory.h
@@ -0,0 +1,19 @@
+//
+//  Observatory.h
+//  Observatory
+//
+//  Created by Peter DeWeese on 12/16/16.
+//  Copyright © 2016 DeWeese Consulting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Observatory.
+FOUNDATION_EXPORT double ObservatoryVersionNumber;
+
+//! Project version string for Observatory.
+FOUNDATION_EXPORT const unsigned char ObservatoryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Observatory/PublicHeader.h>
+
+
