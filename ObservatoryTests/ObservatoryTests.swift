d0aed7a5dc2eba3c9476320191f8ac484c51bfb0
Peter DeWeese <peter@dewee.se>
2016-12-18 18:46:01 -0500

    Added mac target and tests.

--- ObservatoryTests/ObservatoryTests.swift
+++ ObservatoryTests/ObservatoryTests.swift
@@ -19,4 +19,24 @@ class ObservatoryTests: XCTestCase
         objectToWatch["key1"] = "updatedValue1"
         XCTAssert(updateCalled)
     }
+    
+    func testStopObservations()
+    {
+        var updateCalled = false
+        let objectToWatch = NSMutableDictionary(dictionary:["key1":"value1", "key2":"value2"])
+        
+        let observatory = Observatory()
+        observatory.startObservation(objectToWatch, keyPath:"key1")
+        { (oldValue, newValue) in
+            updateCalled = true
+        }
+        
+        XCTAssertNotNil(observatory.observations[objectToWatch, "key1"])
+        XCTAssertFalse(updateCalled)
+        
+        observatory.stopObservations()
+
+        objectToWatch["key1"] = "updatedValue1"
+        XCTAssertFalse(updateCalled)
+    }
 }
