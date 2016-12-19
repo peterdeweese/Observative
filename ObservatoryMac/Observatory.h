9d5afde1321344906a1a05b5f47903dead9f1e32
Peter DeWeese <peter@dewee.se>
2016-12-19 09:13:54 -0500

    Updated names.

--- ObservatoryMac/Observatory.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-
-//! Project version number for Observatory.
-FOUNDATION_EXPORT double ObservatoryVersionNumber;
-
-//! Project version string for Observatory.
-FOUNDATION_EXPORT const unsigned char ObservatoryVersionString[];
--- test/ObservatoryTests.swift
+++ test/ObservatoryTests.swift
@@ -1,5 +1,5 @@
 import XCTest
-@testable import Observatory
+@testable import Observative
 
 class ObservatoryTests: XCTestCase
 {
